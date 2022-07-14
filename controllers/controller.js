688acfc36b8282a583644ce18e3ea5e82b7a48af
OriolGalm <accions018@gmail.com>
2022-07-14 13:16:48 +0200

    Correccions wheels

--- controllers/controller.js
+++ controllers/controller.js
@@ -6,17 +6,29 @@ function submitCar() {
     var brandInput = document.getElementById("brandInput");
     var colorInput = document.getElementById("colorInput");
     //EX1. Validar los campos de matricula (formato: 1234ABC), marca y color, antes de hacer el new Car
-    car = new Car(plateInput.value, colorInput.value, brandInput.value);
     if (/^[0-9]{4}[A-Z]{3}$/.test(plateInput.value) != true) {
         plateInput.style.border = "2px solid red";
+        errores++;
     }
-    else if (brandInput.value == '') {
+    else {
+        plateInput.style.border = "2px solid green";
+    }
+    if (brandInput.value == '') {
+        errores++;
         brandInput.style.border = "2px solid red";
     }
-    else if (colorInput.value == '') {
+    else {
+        brandInput.style.border = "2px solid green";
+    }
+    if (colorInput.value == '') {
+        errores++;
         colorInput.style.border = "2px solid red";
     }
     else {
+        colorInput.style.border = "2px solid green";
+    }
+    if (errores == 0) {
+        car = new Car(plateInput.value, colorInput.value, brandInput.value);
         showVehicle();
         showWheelForm();
     }
@@ -39,41 +51,56 @@ function submitWheelForm() {
         var brandWheel = document.getElementById("brandWheel" + i);
         var diameterWheel = document.getElementById("diameterWheel" + i);
         if (brandWheel.value == '') {
-            errores = 1;
+            errores++;
             brandWheel.style.border = " 2px solid red";
         }
-        else if (diameterWheel.value == '') {
-            errores = 1;
-            diameterWheel.style.border = " 2px solid red";
+        else {
+            brandWheel.style.border = " 2px solid green";
         }
-        else if (parseInt(diameterWheel.value) > 2 || parseInt(diameterWheel.value) < 1) {
-            errores = 1;
+        if (diameterWheel.value == '') {
+            errores++;
             diameterWheel.style.border = " 2px solid red";
         }
         else {
             diameterWheel.style.border = " 2px solid green";
-            brandWheel.style.border = " 2px solid green";
-            var wheel_generica = new Wheel(Number(diameterWheel.value), brandWheel.value);
-            car.addWheel(wheel_generica);
+        }
+        var diameterW = function (x) { return x >= 1 && x <= 2 ? true : false; };
+        if (diameterW(parseInt(diameterWheel.value)) == false) {
+            errores++;
+            diameterWheel.style.border = " 2px solid red";
+        }
+        else {
+            diameterWheel.style.border = " 2px solid green";
         }
     }
     console.log(car);
     if (errores == 0) {
+        for (var i = 1; i < 5; i++) {
+            var brandWheel = document.getElementById("brandWheel" + i);
+            var diameterWheel = document.getElementById("diameterWheel" + i);
+            var wheel_generica = new Wheel(Number(diameterWheel.value), brandWheel.value);
+            car.addWheel(wheel_generica);
+        }
         showWheels();
     }
 }
 function showWheels() {
     //EX4. Optimizar la función showWheels
     var wheelTitle = document.getElementById("wheelTitle");
-    var wheelOutput1 = document.getElementById("wheelOutput1");
-    var wheelOutput2 = document.getElementById("wheelOutput2");
-    var wheelOutput3 = document.getElementById("wheelOutput3");
-    var wheelOutput4 = document.getElementById("wheelOutput4");
+    /* let wheelOutput1 = <HTMLInputElement>document.getElementById("wheelOutput1") ;
+    let wheelOutput2 = <HTMLInputElement>document.getElementById("wheelOutput2");
+    let wheelOutput3 = <HTMLInputElement>document.getElementById("wheelOutput3");
+    let wheelOutput4 = <HTMLInputElement>document.getElementById("wheelOutput4"); */
     wheelTitle.innerText = "Wheels:";
-    wheelOutput1.innerText = "Wheel 1:  " + "Brand: " + car.wheels[0].brand + "  Diameter: " + car.wheels[0].diameter;
+    for (var i = 0; i < car.wheels.length; i++) {
+        var wheelOutput = document.getElementById("wheelOutput".concat(i + 1));
+        wheelOutput.innerText = "Wheel" + [i + 1] + ":  " + "Brand: " + car.wheels[i].brand + "  Diameter: " + car.wheels[i].diameter;
+        console.log("WheelOutput: ", wheelOutput);
+    }
+    /* wheelOutput1.innerText = "Wheel 1:  " + "Brand: " + car.wheels[0].brand + "  Diameter: " + car.wheels[0].diameter;
     wheelOutput2.innerText = "Wheel 2:  " + "Brand: " + car.wheels[1].brand + "  Diameter: " + car.wheels[1].diameter;
     wheelOutput3.innerText = "Wheel 3:  " + "Brand: " + car.wheels[2].brand + "  Diameter: " + car.wheels[2].diameter;
-    wheelOutput4.innerText = "Wheel 4:  " + "Brand: " + car.wheels[3].brand + "  Diameter: " + car.wheels[3].diameter;
+    wheelOutput4.innerText = "Wheel 4:  " + "Brand: " + car.wheels[3].brand + "  Diameter: " + car.wheels[3].diameter; */
 }
 function showWheelForm() {
     var carForm = document.getElementById("create-car-form");
--- controllers/controller.ts
+++ controllers/controller.ts
@@ -8,14 +8,27 @@ function submitCar() {
     let colorInput = <HTMLInputElement>document.getElementById("colorInput");
 
 	//EX1. Validar los campos de matricula (formato: 1234ABC), marca y color, antes de hacer el new Car
-	car = new Car(plateInput.value, colorInput.value, brandInput.value);
+	
     if( /^[0-9]{4}[A-Z]{3}$/.test(plateInput.value) != true){
         plateInput.style.border = "2px solid red";
-    }else if(brandInput.value == ''){
+        errores++;
+    }else{
+        plateInput.style.border = "2px solid green";
+    }
+    if(brandInput.value == ''){
+        errores++;
         brandInput.style.border = "2px solid red";
-    }else if(colorInput.value == ''){
+    }else{
+        brandInput.style.border = "2px solid green";
+    }
+    if(colorInput.value == ''){
+        errores++;
         colorInput.style.border = "2px solid red";
     }else{
+        colorInput.style.border = "2px solid green";
+    }
+    if(errores == 0){
+        car = new Car(plateInput.value, colorInput.value, brandInput.value);
         showVehicle();
         showWheelForm();
     }
@@ -40,34 +53,44 @@ function submitWheelForm() {
 	
 	//EX2. Solo hacer el "new Wheel" si las 4 ruedas son correctas
 	//EX3. Una rueda correcta deberá tener un diámetro entre 1 y 2. Crear una función para validarlas
-
 	for (let i = 1; i <= 4; i++) {
 		let brandWheel = <HTMLInputElement>document.getElementById("brandWheel" + i);
 		let diameterWheel = <HTMLInputElement>document.getElementById("diameterWheel" + i);
         
 
         if(brandWheel.value == '' ){
-            errores = 1;
+            errores++;
             brandWheel.style.border = " 2px solid red"
-        }else if(diameterWheel.value == ''){
-            errores = 1;
-            diameterWheel.style.border = " 2px solid red";
-        }else if(parseInt(diameterWheel.value) > 2 || parseInt(diameterWheel.value) < 1){
-            errores = 1;
+        }else{
+            brandWheel.style.border = " 2px solid green";
+        } 
+        if(diameterWheel.value == ''){
+            errores++;
             diameterWheel.style.border = " 2px solid red";
         }else{
             diameterWheel.style.border = " 2px solid green";
-            brandWheel.style.border = " 2px solid green";
+        } 
+
+        const diameterW = (x:Number): Boolean => x >= 1 && x <= 2 ? true : false;
+        
+        if(diameterW(parseInt(diameterWheel.value)) == false){
+            errores++;
+            diameterWheel.style.border = " 2px solid red";
+        }else{
+            diameterWheel.style.border = " 2px solid green";
+        }
+    }    
+
+    console.log(car)
+    if(errores == 0){
+        for(let i = 1; i< 5; i++){
+            let brandWheel = <HTMLInputElement>document.getElementById("brandWheel" + i);
+            let diameterWheel = <HTMLInputElement>document.getElementById("diameterWheel" + i);
             let wheel_generica = new Wheel(Number(diameterWheel.value), brandWheel.value);
             car.addWheel(wheel_generica);
-        }     
-         
-	}
-	console.log(car)
-    if(errores == 0){
+        }
 	    showWheels();
     }
-    
 }
 
 function showWheels() {
@@ -75,17 +98,23 @@ function showWheels() {
 	//EX4. Optimizar la función showWheels
     let wheelTitle = <HTMLInputElement>document.getElementById("wheelTitle");
     
-    let wheelOutput1 = <HTMLInputElement>document.getElementById("wheelOutput1") ;
+    /* let wheelOutput1 = <HTMLInputElement>document.getElementById("wheelOutput1") ;
     let wheelOutput2 = <HTMLInputElement>document.getElementById("wheelOutput2");
     let wheelOutput3 = <HTMLInputElement>document.getElementById("wheelOutput3");
-    let wheelOutput4 = <HTMLInputElement>document.getElementById("wheelOutput4");
+    let wheelOutput4 = <HTMLInputElement>document.getElementById("wheelOutput4"); */
     
     
     wheelTitle.innerText = "Wheels:";
-    wheelOutput1.innerText = "Wheel 1:  " + "Brand: " + car.wheels[0].brand + "  Diameter: " + car.wheels[0].diameter;
+    for(let i = 0; i<car.wheels.length; i++){
+        let wheelOutput = <HTMLInputElement>document.getElementById(`wheelOutput${i + 1}`);
+        
+        wheelOutput.innerText = "Wheel" + [i + 1] + ":  " + "Brand: " + car.wheels[i].brand + "  Diameter: " + car.wheels[i].diameter;
+        console.log("WheelOutput: ", wheelOutput);
+    }
+    /* wheelOutput1.innerText = "Wheel 1:  " + "Brand: " + car.wheels[0].brand + "  Diameter: " + car.wheels[0].diameter;
     wheelOutput2.innerText = "Wheel 2:  " + "Brand: " + car.wheels[1].brand + "  Diameter: " + car.wheels[1].diameter;
     wheelOutput3.innerText = "Wheel 3:  " + "Brand: " + car.wheels[2].brand + "  Diameter: " + car.wheels[2].diameter;
-    wheelOutput4.innerText = "Wheel 4:  " + "Brand: " + car.wheels[3].brand + "  Diameter: " + car.wheels[3].diameter;
+    wheelOutput4.innerText = "Wheel 4:  " + "Brand: " + car.wheels[3].brand + "  Diameter: " + car.wheels[3].diameter; */
     
 }
 
